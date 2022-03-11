6028bd8a1c3a50d9228b87ca24a109ee7df633b8
Giao Ho <joutvhu@gmail.com>
2022-03-11 12:21:47 +0700

    Fix icon path

--- src/main/java/com/joutvhu/intellij/dartscripts/util/DartIcons.java
+++ src/main/java/com/joutvhu/intellij/dartscripts/util/DartIcons.java
@@ -12,10 +12,10 @@ public class DartIcons implements Serializable {
     private static final long serialVersionUID = -7782889778971848655L;
 
     @NotNull
-    public final Icon ScriptIcon = load("icons/dartScript.svg", -7782889778971848656L, 0);
+    public final Icon ScriptIcon = load("/icons/dartScript.svg");
 
     @NotNull
-    private Icon load(@NotNull String path, long cacheKey, int flags) {
-        return IconManager.getInstance().loadRasterizedIcon(path, DartIcons.class.getClassLoader(), cacheKey, flags);
+    private Icon load(@NotNull String path) {
+        return IconManager.getInstance().getIcon(path, DartIcons.class);
     }
 }
